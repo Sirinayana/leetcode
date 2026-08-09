4e6931177c60d4fef0d1f0246a30f65089557b76
Sirinayana <peddapellysirinayana@gmail.com>
2026-08-10 00:58:04 +0530

    Time: 0 ms (100%), Space: 19.2 MB (56.86%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        k=0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k]=nums[i]
+                k+=1
+        return k
+nums=[0,1,2,2,3,0,4,2]
+val=2
+solution1=Solution()
+print(solution1.removeElement(nums,val))
+        
