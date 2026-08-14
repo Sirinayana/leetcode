639e5a0672acace8343e9ab569d2b7688547fd44
Sirinayana <peddapellysirinayana@gmail.com>
2026-08-15 00:20:26 +0530

    Time: 4 ms (45.7%), Space: 20.5 MB (18.17%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,8 +2,8 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n=len(nums)
         total=n*(n+1)//2
-        missing_number=total-sum(nums)
-        return missing_number
+        missing=total-sum(nums)
+        return missing
 nums=[3,0,1]
 solution=Solution()
 print(solution.missingNumber(nums))
