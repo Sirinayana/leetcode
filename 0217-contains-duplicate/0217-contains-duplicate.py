60bb0a3f0cdbbb16ec0f70e9e88e1a3790841e38
Sirinayana <peddapellysirinayana@gmail.com>
2026-08-15 00:02:59 +0530

    Time: 15 ms (59.06%), Space: 37.5 MB (6.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,14 +1,13 @@
 class Solution:
-    def containsDuplicate(self, nums):
+    def containsDuplicate(self, nums: List[int]) -> bool:
         d={}
-        for num in nums:
-            if num in d:
+        for i in nums:
+            if i in d:
                 return True
-            d[num]=1
+            d[i]=1
+        
         return False
-nums = [1,2,3,4]
-solution = Solution()
+nums=[1,2,3,1]
+solution=Solution()
 print(solution.containsDuplicate(nums))
-
-
         
