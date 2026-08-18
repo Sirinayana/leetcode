d6b2281901ba9fdb04b2eff499d9d99a30f0388b
Sirinayana <peddapellysirinayana@gmail.com>
2026-08-18 22:59:17 +0530

    Time: 12 ms (56.22%), Space: 19.5 MB (30.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        d={}
+        f1={}
+        f2={}
         for i in s:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        for i in t:
-            if i not in d:
-                return False
-            d[i]=d[i]-1
-        if d[i]<0:
-            return False
-        for value in d.values():
-            if value!=0:
-                return False
-        return True
-s="anagram"
-t="nagaram"
-solution1=Solution()
-print(solution1.isAnagram(s,t))
-        
+            f1[i]=f1.get(i,0)+1
+        for j in t:
+            f2[j]=f2.get(j,0)+1
+        if f1==f2:
+            return True
+        return False
+s = "anagram"
+t = "nagaram"
+solution=Solution()
+print(solution.isAnagram(s,t))
+
+
         
