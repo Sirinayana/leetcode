565b1bd69175861691dde45155d317b9a41fe6d8
Sirinayana <peddapellysirinayana@gmail.com>
2026-08-24 23:18:54 +0530

    Time: 7 ms (81.07%), Space: 19.8 MB (34.19%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        clean = ""
+
+        for ch in s:
+            if ch.isalnum():
+                clean += ch.lower()
+
+        return clean == clean[::-1]
+
+
+s = "race a car"
+
+solution = Solution()
+print(solution.isPalindrome(s))
+
+
+
+        
+        
